c59f1cf9c7beeaf5a79b36c4b47cf435112f2913
Tarun Goenka <tarun.goenka12@gmail.com>
2018-04-18 09:53:45 +0530

    Create form.php

---
+++ form.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+echo "<br>";
+if($_SESSION['voted']==0){
+
+	include 'connection.php';
+	$query = "select * from candidates where section='$_SESSION[section]'";
+	$result = mysqli_query($dbc,$query) or die("Querying error");
+
+	echo '<form id="vinitha" method="post">';
+	$n=mysqli_num_rows($result);
+	for($i=1;$i<=$n;$i++)
+	{
+		$res = mysqli_fetch_array($result);
+		echo "<input type=radio id='option1' name='rad' value=".$res['name'].">".$res['name']."</input><br />";
+	}
+	echo "</form>";
+
+}
+else {
+	echo "<br><p style='color:red;'>You have already voted</p>";
+}
+
+
+?>
